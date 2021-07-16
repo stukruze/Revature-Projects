2893e68ac9fbeeebdabbff7da2ab3e4d0a4e6a1f
stukruze <audiolaboratory@hotmail.com>
2021-07-16 14:48:04 -0400

    adding project1

---
+++ Project1/src/main/java/com/stuartkruze/util/HibernateUtil.java
@@ -0,0 +1,15 @@
+package com.stuartkruze.util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+	
+	private static SessionFactory sf = new Configuration().configure().buildSessionFactory();
+
+	public static Session getSession() {
+		return sf.openSession();
+	}
+	
+}
